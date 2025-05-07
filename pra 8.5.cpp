7945f76d061fad004569e3a1cb6afdfeefc4e712
Jaychheta <jaychheta947@gmail.com>
2025-05-07 12:06:53 +0530

    Update pra 8.5.cpp

--- pra 8.5.cpp
+++ pra 8.5.cpp
@@ -8,24 +8,39 @@ using namespace std;
 
 class DirectoryManager {
 private:
-    map<string, vector<string>> directory;  // folder -> list of files
+    map<string, vector<string>> directory;
 
 public:
-    void addFolder(const string &folderName) {
-        directory[folderName];  // creates empty folder if not exist
+    void addFolder(const string& folderName) {
+        if (directory.find(folderName) == directory.end()) {
+            directory[folderName];
+            cout << "Folder '" << folderName << "' added successfully!" << endl;
+        } else {
+            cout << "Folder '" << folderName << "' already exists." << endl;
+        }
     }
 
-    void addFileToFolder(const string &folderName, const string &fileName) {
-        directory[folderName].push_back(fileName);  // adds file to folder
+    void addFileToFolder(const string& folderName, const string& fileName) {
+        if (directory.find(folderName) != directory.end()) {
+            directory[folderName].push_back(fileName);
+            cout << "File '" << fileName << "' added to folder '" << folderName << "'." << endl;
+        } else {
+            cout << "Error: Folder '" << folderName << "' does not exist!" << endl;
+        }
     }
 
     void displayDirectory() {
-        cout << "Directory Structure:" << endl;
-        for (auto it = directory.begin(); it != directory.end(); ++it) {
-            cout << "Folder: " << it->first << endl;
-            vector<string> files = it->second;
-            sort(files.begin(), files.end());  // optional: sort files alphabetically
-            for (const string &file : files) {
+        cout << "\nDirectory Structure:" << endl;
+        if (directory.empty()) {
+            cout << "No folders to display." << endl;
+            return;
+        }
+
+        for (auto& folder : directory) {
+            cout << "Folder: " << folder.first << endl;
+            vector<string> files = folder.second;
+            sort(files.begin(), files.end());
+            for (const string& file : files) {
                 cout << "  - " << file << endl;
             }
         }
@@ -34,17 +49,41 @@ public:
 
 int main() {
     DirectoryManager dm;
+    int choice;
+    string folderName, fileName;
 
-    dm.addFolder("src");
-    dm.addFolder("docs");
-    dm.addFolder("tests");
+    do {
+        cout << "\n1. Add Folder\n2. Add File to Folder\n3. Display Directory\n4. Exit\nEnter your choice: ";
+        cin >> choice;
+        cin.ignore();
 
-    dm.addFileToFolder("src", "main.cpp");
-    dm.addFileToFolder("src", "utils.cpp");
-    dm.addFileToFolder("docs", "readme.md");
-    dm.addFileToFolder("tests", "test_main.cpp");
+        switch (choice) {
+            case 1:
+                cout << "Enter folder name: ";
+                getline(cin, folderName);
+                dm.addFolder(folderName);
+                break;
 
-    dm.displayDirectory();
+            case 2:
+                cout << "Enter folder name: ";
+                getline(cin, folderName);
+                cout << "Enter file name: ";
+                getline(cin, fileName);
+                dm.addFileToFolder(folderName, fileName);
+                break;
+
+            case 3:
+                dm.displayDirectory();
+                break;
+
+            case 4:
+                cout << "Exiting the program." << endl;
+                break;
+
+            default:
+                cout << "Invalid choice, please try again." << endl;
+        }
+    } while (choice != 4);
 
     return 0;
 }
