add9169364d9fa1b0b5033bf58c5776b74f7d7f8
Jaychheta <jaychheta947@gmail.com>
2025-05-07 11:36:43 +0530

    Update pra 5.4.cpp

--- pra 5.4.cpp
+++ pra 5.4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Fahrenheit; // Forward declaration
+class Fahrenheit;
 
 class Celsius {
     float temp;
@@ -9,10 +9,7 @@ public:
     Celsius(float t = 0) { temp = t; }
     float getTemp() { return temp; }
 
-    // Convert Celsius to Fahrenheit
     operator Fahrenheit();
-
-    // Equality check
     bool operator==(Fahrenheit f);
 };
 
@@ -22,32 +19,34 @@ public:
     Fahrenheit(float t = 0) { temp = t; }
     float getTemp() { return temp; }
 
-    // Convert Fahrenheit to Celsius
     operator Celsius() {
         return Celsius((temp - 32) * 5 / 9);
     }
 
-    // Equality check
     bool operator==(Celsius c) {
         return ((temp - 32) * 5 / 9) == c.getTemp();
     }
 };
 
-// Define conversion outside class
 Celsius::operator Fahrenheit() {
     return Fahrenheit((temp * 9 / 5) + 32);
 }
 
-// Equality check from Celsius side
 bool Celsius::operator==(Fahrenheit f) {
     return ((temp * 9 / 5) + 32) == f.getTemp();
 }
 
 int main() {
-    Celsius c1(37);
+    float cTemp, fTemp;
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> cTemp;
+    Celsius c1(cTemp);
     Fahrenheit f1 = c1;
 
-    Fahrenheit f2(98.6);
+    cout << "Enter temperature in Fahrenheit: ";
+    cin >> fTemp;
+    Fahrenheit f2(fTemp);
     Celsius c2 = f2;
 
     if (c1 == f2)
@@ -55,7 +54,6 @@ int main() {
     else
         cout << "Temperatures are not equal\n";
 
-    // Static array to store converted Fahrenheit objects
     Fahrenheit fArray[2];
     fArray[0] = f1;
     fArray[1] = Fahrenheit(100);
